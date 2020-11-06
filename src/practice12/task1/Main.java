3da0dcd91ee824ed232a8396d07b6dfb5b292234
OldBikerS <53240042+OldBikerS@users.noreply.github.com>
2020-11-06 04:33:30 +0300

    Add files via upload

--- src/practice12/task1/Main.java
+++ src/practice12/task1/Main.java
@@ -1,23 +1,18 @@
 package practice12.task1;
 
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
 import javafx.scene.layout.FlowPane;
 import javafx.scene.layout.Region;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
-import java.lang.Math;
 
 public class Main extends Application {
-    private static Planet mercury = new Planet ( "Mercury" , 3.285 * Math.pow ( 10 , 23 ) , 2440 );
-    private static Planet venus = new Planet ( "Venus" , 4.867 * Math.pow ( 10 , 24 ) , 6052 );
-    private static Planet earth = new Planet ( "Earth" , 5.972 * Math.pow ( 10 , 24 ) , 6371 );
-    private static Planet mars = new Planet ( "Mars" , 6.39 * Math.pow ( 10 , 23 ) , 3390 );
-    private static Planet jupiter = new Planet ( "Jupiter" , 1.898 * Math.pow ( 10 , 27 ) , 69911 );
-    private static Planet saturn = new Planet ( "Saturn" , 5.683 * Math.pow ( 10 , 26 ) , 58232 );
-    private static Planet uranus = new Planet ( "Uranus" , 8.681 * Math.pow ( 10 , 25 ) , 25362 );
-    private static Planet neptune = new Planet ( "Neptune" , 1.024 * Math.pow ( 10 , 26 ) , 24622 );
     private static FlowPane root;
 
     public static void main ( String[] args ) {
@@ -39,20 +34,24 @@ public class Main extends Application {
     }
 
     private void setPlanets ( ) {
-        root.getChildren ( ).add ( new Text ( "Planets:" ) );
+        root.getChildren ( ).add ( new Text( "Planets:" ) );
 
-        Region lineBreak = new Region ( );
-        lineBreak.setPrefSize ( Double.MAX_VALUE , 0.0 );
-        root.getChildren ( ).add ( lineBreak );
+        Region lineBreak = new Region();
+        lineBreak.setPrefSize(Double.MAX_VALUE, 0.0);
+        root.getChildren().add (lineBreak);
 
-        root.getChildren ( ).add ( mercury );
-        root.getChildren ( ).add ( venus );
-        root.getChildren ( ).add ( earth );
-        root.getChildren ( ).add ( mars );
-        root.getChildren ( ).add ( jupiter );
-        root.getChildren ( ).add ( saturn );
-        root.getChildren ( ).add ( uranus );
-        root.getChildren ( ).add ( neptune );
-
+        for(Planet planet : Planet.values()) {
+            Button planetButton = new Button();
+            planetButton.setText(planet.name);
+            planetButton.setOnAction ( new EventHandler<ActionEvent>( ) {
+                public void handle ( ActionEvent event ) {
+                    Alert alert = new Alert ( Alert.AlertType.INFORMATION );
+                    alert.setHeaderText ( "Gravitation " + planet.name );
+                    alert.setContentText ( String.valueOf ( planet.gravitation ) );
+                    alert.show ( );
+                }
+            } );
+            root.getChildren().add(planetButton);
+        }
     }
 }
--- src/practice12/task1/Planet.java
+++ src/practice12/task1/Planet.java
@@ -5,28 +5,27 @@ import javafx.event.EventHandler;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 
-public class Planet extends Button {
+public enum Planet {
+    Mercury("Mercury" , 3.285 * Math.pow ( 10 , 23 ) , 2440),
+    Venus("Venus" , 4.867 * Math.pow ( 10 , 24 ) , 6052 ),
+    Earth("Earth" , 5.972 * Math.pow ( 10 , 24 ) , 6371 ),
+    Mars( "Mars" , 6.39 * Math.pow ( 10 , 23 ) , 3390 ),
+    Jupiter ( "Jupiter" , 1.898 * Math.pow ( 10 , 27 ) , 69911 ),
+    Saturn ("Saturn" , 5.683 * Math.pow ( 10 , 26 ) , 58232 ),
+    Uranus ("Uranus" , 8.681 * Math.pow ( 10 , 25 ) , 25362 ),
+    Neptune("Neptune" , 1.024 * Math.pow ( 10 , 26 ) , 24622 );
 
-    private final double G = 0.0000000000667; // Gravitational Constant
+    private final double G = 0.0000000000667;
+    public String name;
+    public double mass;
+    public double radius;
+    public double gravitation;
 
-    private double mass;
-    private double radius;
-    private double gravitation;
-
-    public Planet ( final String name , double mass , double radius ) {
-        this.setText ( name );
+    Planet(final String name , double mass , double radius ) {
+        this.name = name;
         this.mass = mass;
         this.radius = radius;
-        setGravitation ( );
-
-        setOnAction ( new EventHandler < ActionEvent > ( ) {
-            public void handle ( ActionEvent event ) {
-                Alert alert = new Alert ( Alert.AlertType.INFORMATION );
-                alert.setHeaderText ( "Gravitation " + name );
-                alert.setContentText ( String.valueOf ( gravitation ) );
-                alert.show ( );
-            }
-        } );
+        setGravitation();
     }
 
     private void setGravitation ( ) {
